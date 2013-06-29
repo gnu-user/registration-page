2a2a114e2c4a1f7ca62aef6c13693abc68df2356
gnu_user <gsoc.student@gmail.com>
2013-06-29 14:59:52 -0400

    Fixed a dispay issue for the navbar when the browser windows is small

--- templates/header.php
+++ templates/header.php
@@ -27,17 +27,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Registration Page">
     <meta name="author" content="Computer Science Club">
-
     <!-- CSS styles -->
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 20px;
+      }
+    </style>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
-
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="js/html5shiv.min.js"></script>
     <![endif]-->
-  
   </head>
   <body>
     <div class="navbar navbar-inverse navbar-fixed-top">
