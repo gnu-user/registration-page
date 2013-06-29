dab568e262f527052f0f3970ac55f5daad3e5522
gnu_user <gsoc.student@gmail.com>
2013-06-29 15:00:31 -0400

    Updated the footer to link to cs-club website and github page

--- templates/footer.php
+++ templates/footer.php
@@ -21,10 +21,11 @@
 ?>
       <footer class="footer">
         <div class="pull-left">
-          <p>Designed and built by <a target="_blank" href="https://github.com/gnu-user/registration-page">Computer Science Club</a></p>
+          <p>Designed and built by the <a target="_blank" href="http://cs-club.ca">Computer Science Club</a></p>
           <p>This website is Open Source, code licensed under the 
             <a target="_blank" href="http://www.gnu.org/licenses/agpl-3.0.html">GNU Affero General Public License, Version 3</a>
           </p>
+          <p>Contribute to this website, view the source code <a target="_blank" href="https://github.com/CS-CLUB/registration-page">@github</a>
           <p><strong>&copy; Computer Science Club</strong></p>
         </div>
       </footer>
