f1e37e67c47b296f43e3ee8516960f24d51c5378
gnu_user <gsoc.student@gmail.com>
2013-09-15 00:40:06 -0400

    Fixed the mailing list subscription, the old method no longer works

--- scripts/welcome-email.sh
+++ scripts/welcome-email.sh
@@ -26,13 +26,7 @@ EMAIL_MESSAGE="<html><body>
 at UOIT and DC. If you would like to stay up to date with club events please feel free to 
 subscribe to the club mailing list below or chat with us on IRC.</p>
 
-<legend><h4>Subscribe to the CS Club Mailing List</h4></b></legend></p>
-<form style="border:1px solid #ccc;padding:3px;text-align:left;width:325px;" method="post" action="http://groups.google.com/group/uoit_csc/boxsubscribe">
-<input type="hidden" name="hl" value="en" /><br />
-&nbsp;&nbsp;&nbsp;<b>E-mail:    </b><input style="border:1px solid #ccc;" type="text" name="email" />
-<input style="padding:3px 4px;-webkit-border-radius:2px 2px;border:solid 1px rgb(153, 153, 153);background:-webkit-gradient(linear, 0% 0%, 0% 100%, from(rgb(255, 255, 255)), to(rgb(221, 221, 221)));color: #333;text-decoration:none;cursor:pointer;display:inline-block;text-align:center;text-shadow:0px 1px 1px rgba(255,255,255,1);line-height:1;" type="submit" name="sub" value="Subscribe" />
-<br /><br /></form>
-<p><a href="http://groups.google.com/group/uoit_csc?hl=en">Visit this group</a><br /></p>
+<p><a href="https://groups.google.com/forum/#!forum/uoit_csc/join">Subscribe to the Mailing List!</a><br /></p>
 
 <p><h4>IRC Server Details</h4></p>
 <ul><li><b>Server:</b> cs-club.ca</li>
